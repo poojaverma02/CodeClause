1bf93332a8357c6889bce8dbe6da4e5a3816fc7f
poojaverma02 <poojaverma.ece.aec@gmail.com>
2023-09-16 15:55:48 +0530

    Files Added

---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key":
+      process.env.REACT_APP_YOUTUBE_API_KEY ||
+      "fa4857e199msh40d8fbda2d33630p1350b5jsne6efac2076c3",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
